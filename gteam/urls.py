e4bcdbf5591bd5a1096f0c4dcc410c76834b675d
KantaNakashima <fko2347007@stu.o-hara.ac.jp>
2024-10-31 10:37:37 +0900

    10/31 10:37

--- gteam/urls.py
+++ gteam/urls.py
@@ -2,12 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ログイン前ホーム画面
     path("", views.index, name="index"),
+    # ログイン画面
     path("userlogin", views.userlogin, name="userlogin"),
+    # ログイン後ホーム画面
     path("mainmenu", views.mainmenu, name="mainmenu"),
+    # 新規登録画面
     path("usersignup", views.usersignup, name="usersignup"),
-    path("reservationchange", views.reservationchange, name="reservationchange"),
+    # 予約画面
+    path("cleaningappointment", views.cleaningappointment, name="cleaningappointment"),
+    # 予約完了画面
     path("reservationcompleted", views.reservationcompleted, name="reservationcompleted"),
+    # 予約詳細画面
     path("userreservationdetails", views.userreservationdetails, name="userreservationdetails"),
+    # 予約一覧画面
     path("userreservationlistscreen", views.userreservationlistscreen, name="userreservationlistscreen"),
 ]
--- gteam/views.py
+++ gteam/views.py
@@ -1,25 +1,33 @@
 from django.shortcuts import render
 
+# ログイン前ホーム画面
 def index(request):
     return render(request, 'index.html')
 
+# ログイン画面
 def userlogin(request):
     return render(request, 'userlogin.html')
 
+# ログイン後ホーム画面
 def mainmenu(request):
     return render(request, 'mainmenu.html')
 
+# 新規登録画面
 def usersignup(request):
     return render(request, 'usersignup.html')
 
-def reservationchange(request):
-    return render(request, 'reservationchange.html')
+# 予約画面
+def cleaningappointment(request):
+    return render(request, 'cleaningappointment.html')
 
+# 予約完了画面
 def reservationcompleted(request):
     return render(request, 'reservationcompleted.html')
 
+# 予約詳細画面
 def userreservationdetails(request):
     return render(request, 'userreservationdetails.html')
 
+# 予約一覧画面
 def userreservationlistscreen(request):
     return render(request, 'userreservationlistscreen.html')
